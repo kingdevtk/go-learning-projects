5597a650ac8ffb0c10bfe22b9651f49fe6f0ff62
kingdevtk <trina.t.king@gmail.com>
2023-02-14 16:00:42 -0500

    booking example

--- booking-app/main.go
+++ booking-app/main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	bookingType := "Studio Time"
+	const timeSlots = 10
+	remainingSlots := 10
+
+	fmt.Printf("Welcome to the %v booking application!\n", bookingType)
+	fmt.Printf("We have a total of %v slots, and %v slots still available.\n", timeSlots, remainingSlots)
+	fmt.Printf("Reserve your time slot here.\n ")
+
+	var userName string
+	var timeBlocks int
+	// ask user for their name
+
+	userName = "Solange"
+	timeBlocks = 3
+	fmt.Printf("User %v booked %v timeslots.\n", userName, timeBlocks)
+
 }
